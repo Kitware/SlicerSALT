58b0ec306b4324b7bd3083e4dd89b66a0f67a1e8
Julien Finet <julien.finet@kitware.com>
2013-08-23 15:10:21 -0400

    Improve the dashboard driver and template scripts

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -21,6 +21,6 @@ set(CTEST_NIGHTLY_START_TIME "3:00:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 # See this issue for more details: http://public.kitware.com/Bug/view.php?id=12555
-set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_SITE "open.cdash.org")
 set(CTEST_DROP_LOCATION "/submit.php?project=${CTEST_PROJECT_NAME}")
 set(CTEST_DROP_SITE_CDASH TRUE)
