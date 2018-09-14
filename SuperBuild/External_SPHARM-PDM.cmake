fa349f46c2b3906ccc7cd390a889ad479ecc28f8
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-09-14 01:11:28 -0400

    cmake: Update Slicer/GROUPS/SPHARM-PDM to fix macOS packaging of fortran libraries
    List of Slicer changes:
    
    $ git shortlog 109d50f88..92726ca29 --no-merges
    jcfr (3):
          [Backport] STYLE: Add comments to SlicerExtensionCPack and SlicerCPack modules
          [Backport] ENH: Support fix-up of shared third-party libraries installed by extension.
          [Backport] COMP: Exclude system library when updating EXTENSION_BUNDLE_FIXUP_LIBRARY_DIRECTORIES
    
    
    List of GROUPS changes:
    
    $ git shortlog c2aeef9..eccc120 --no-merges
    Jean-Christophe Fillion-Robin (1):
          cmake: Ensure fortran libraries can be packaged on macOS
    
    
    List of SPHARM-PDM changes:
    
    $ git shortlog d3f8b24..c6683a7 --no-merges
    Jean-Christophe Fillion-Robin (1):
          cmake: Ensure fortran libraries can be packaged on macOS

--- SuperBuild/External_SPHARM-PDM.cmake
+++ SuperBuild/External_SPHARM-PDM.cmake
@@ -54,7 +54,7 @@ if(NOT DEFINED ${proj}_DIR AND NOT ${CMAKE_PROJECT_NAME}_USE_SYSTEM_${proj})
   ExternalProject_Add(${proj}
     ${${proj}_EP_ARGS}
     GIT_REPOSITORY "${EP_GIT_PROTOCOL}://github.com/jcfr/SPHARM-PDM.git"
-    GIT_TAG "d3f8b242a467be8616ccc8d611a734d931ef49a6"
+    GIT_TAG "c6683a7a80ea21b2a103445ccdf8dd99ce777176" # slicersalt-2018-09-11-08eaaed1f
     SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
     BINARY_DIR ${${proj}_DIR}
     INSTALL_COMMAND ${CMAKE_COMMAND} --build ${${proj}_PACKAGE_DIR} --config ${config} --target package
