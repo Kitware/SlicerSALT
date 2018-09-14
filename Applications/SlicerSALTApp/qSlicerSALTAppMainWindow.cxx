d86099f00a07328c1098867f5041f04d2b01b9dd
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2018-09-14 11:39:31 -0400

    Update License
    See #106

--- Applications/SlicerSALTApp/qSlicerSALTAppMainWindow.cxx
+++ Applications/SlicerSALTApp/qSlicerSALTAppMainWindow.cxx
@@ -1,19 +1,5 @@
-/*==============================================================================
-
-  Copyright (c) Kitware, Inc.
-
-  See http://www.slicer.org/copyright/copyright.txt for details.
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an "AS IS" BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-
-  This file was originally developed by Julien Finet, Kitware, Inc.
-  and was partially funded by NIH grant 3P41RR013218-12S1
-
-==============================================================================*/
+/* Distributed under the OSI-approved BSD 3-Clause License.  See accompanying
+   file LICENSE.  */
 
 // Qt includes
 #include <QDebug>
