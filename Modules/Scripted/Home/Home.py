b097199d23172fa8dba41c1606cabf8bca928f42
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-04-22 23:18:58 -0400

    ENH: Simplify Home module deriving from QTextBrowser.
    See #93

--- Modules/Scripted/Home/Home.py
+++ Modules/Scripted/Home/Home.py
@@ -55,43 +55,26 @@ class HomeWidget(ScriptedLoadableModuleWidget):
 <br>
 The drop-down Modules are ordered to follow the basic workflow for choosing and using data.  As a quick reference, the basic steps involve:<br>
 <br>
-&nbsp; 1. Use the <a href="#"><b>Data importer</b></a> module to load your segmentations from FreeSurf, FSL, Autoseg, or a bunch of vtp's<br><br>
-&nbsp; 2. Use <a href="#"><b>Shape Population Viewer</b></a> to do a quality check on the imported data<br><br>
-&nbsp; 3. Use <a href="#"><b>SPHARM Shape Analysis Module</b></a> to do spherical harmonics based analysis<br><br>
-&nbsp; 4. Use the <a href="#"><b>Study-specific Shape Analysis</b></a> module.<br><br>
-&nbsp; 5. Use the <a href="#"><b>S-Rep Shape Analysis</b></a> module to do shape analysis via skeletal representations.<br><br>
-&nbsp; 6. Use the <a href="#"><b>Shape Evaluator</b></a> module to compute a mean shape and see how the population varies.<br><br>
-&nbsp; 7. Use <a href="#"><b>Shape Regressions</b></a> module to do regression based analysis.<br><br>
-&nbsp; 8. Use the <a href="#"><b>Shape Statistics</b></a> module.<br><br>
+&nbsp; 1. Use the <a href="#DataImporter"><b>Data importer</b></a> module to load your segmentations from FreeSurf, FSL, Autoseg, or a bunch of vtp's<br><br>
+&nbsp; 2. Use <a href="#ShapePopulationViewer"><b>Shape Population Viewer</b></a> to do a quality check on the imported data<br><br>
+&nbsp; 3. Use <a href="#ShapeAnalysisModule"><b>SPHARM Shape Analysis Module</b></a> to do spherical harmonics based analysis<br><br>
+&nbsp; 4. Use the <a href="#GroupWiseRegistrationModule"><b>Study-specific Shape Analysis</b></a> module.<br><br>
+&nbsp; 5. Use the <a href="#SkeletalRepresentationVisualizer"><b>S-Rep Shape Analysis</b></a> module to do shape analysis via skeletal representations.<br><br>
+&nbsp; 6. Use the <a href="#Home"><b>Shape Evaluator</b></a> module to compute a mean shape and see how the population varies.<br><br>
+&nbsp; 7. Use <a href="#RegressionComputation"><b>Shape Regressions</b></a> module to do regression based analysis.<br><br>
+&nbsp; 8. Use the <a href="#Home"><b>Shape Statistics</b></a> module.<br><br>
 """
 
         # TEXTEDIT
-        self.HomeTextSection = qt.QTextEdit()
-        self.HomeTextSection.setReadOnly(True)
-        self.HomeTextSection.setText(text)
+        self.HomeTextSection = qt.QTextBrowser()
+        self.HomeTextSection.setHtml(text)
         self.HomeTextSection.setMinimumHeight(400)
-        self.HomeTextSection.connect('cursorPositionChanged()', self.slot)
+        self.HomeTextSection.connect('anchorClicked(QUrl)', self.onAnchorClicked)
         self.layout.addWidget(self.HomeTextSection)
 
         # SPACER
         self.layout.addStretch()
 
-    def slot(self):
-        pos = self.HomeTextSection.textCursor().position()
-
-        if pos >= 181 and pos <= 194 :
-            slicer.util.selectModule(slicer.moduleNames.DataImporter)
-        elif pos >= 288 and pos <= 311 :
-            slicer.util.selectModule(slicer.moduleNames.ShapePopulationViewer)
-        elif pos >= 365 and pos <= 393 :
-            slicer.util.selectModule(slicer.moduleNames.ShapeAnalysisModule)
-        elif pos >= 449 and pos <= 478 :
-            slicer.util.selectModule(slicer.moduleNames.GroupWiseRegistrationModule)
-        elif pos >= 501 and pos <= 522 :
-            slicer.util.selectModule(slicer.moduleNames.SkeletalRepresentationVisualizer)
-        elif pos >= 594 and pos <= 610 :
-            slicer.util.selectModule(slicer.moduleNames.Home) #SVA
-        elif pos >= 686 and pos <= 703 :
-            slicer.util.selectModule(slicer.moduleNames.RegressionComputation)
-        elif pos >= 758 and pos <= 774 :
-             slicer.util.selectModule(slicer.moduleNames.Home) #MFSDA
+    def onAnchorClicked(self, url):
+        moduleName = url.fragment()
+        slicer.util.selectModule(moduleName)
