a42de30fb8a5b91de8b03886d4b7fe27cad6a8d6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2019-04-23 01:02:35 -0400

    ENH: Add ShapeVariationAnalyzer module.
    See #48
    
    List of changes specific to slicersalt fork:
    
    $ git shortlog 8340af2..4234377 --no-merges
    Jean-Christophe Fillion-Robin (1):
          STYLE: Update python scripts for python 3.x

---
+++ SuperBuild/External_python-ShapeVariationAnalyzer-requirements.cmake
@@ -0,0 +1,43 @@
+set(proj python-ShapeVariationAnalyzer-requirements)
+
+# Set dependency list
+set(${proj}_DEPENDENCIES python NUMPY python-pip)
+
+if(NOT DEFINED Slicer_USE_SYSTEM_${proj})
+  set(Slicer_USE_SYSTEM_${proj} ${Slicer_USE_SYSTEM_python})
+endif()
+
+# Include dependent projects if any
+ExternalProject_Include_Dependencies(${proj} PROJECT_VAR proj DEPENDS_VAR ${proj}_DEPENDENCIES)
+
+if(Slicer_USE_SYSTEM_${proj})
+  foreach(module_name IN ITEMS sklearn)
+    ExternalProject_FindPythonPackage(
+      MODULE_NAME "${module_name}"
+      REQUIRED
+      )
+  endforeach()
+endif()
+
+if(NOT Slicer_USE_SYSTEM_${proj})
+
+  ExternalProject_Add(${proj}
+    ${${proj}_EP_ARGS}
+    DOWNLOAD_COMMAND ""
+    SOURCE_DIR ${CMAKE_BINARY_DIR}/${proj}
+    BUILD_IN_SOURCE 1
+    CONFIGURE_COMMAND ""
+    BUILD_COMMAND ""
+    INSTALL_COMMAND ${PYTHON_EXECUTABLE} -m pip install -r ${SlicerSALT_SOURCE_DIR}/SuperBuild/ShapeVariationAnalyzer-requirements.txt
+    LOG_INSTALL 1
+    DEPENDS
+      ${${proj}_DEPENDENCIES}
+    )
+
+  ExternalProject_GenerateProjectDescription_Step(${proj}
+    VERSION ${_version}
+    )
+
+else()
+  ExternalProject_Add_Empty(${proj} DEPENDS ${${proj}_DEPENDENCIES})
+endif()
